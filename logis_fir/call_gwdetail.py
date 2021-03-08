98a6e0e23923a5946dad84566a01117a3192c304
Syiiiii <74089477+Syiiiii@users.noreply.github.com>
2021-03-08 21:48:06 +0800

    项目管理完善(暂缺word文件的存储功能/项目列表搜索功能)
    @2021/3/8

--- logis_fir/call_gwdetail.py
+++ logis_fir/call_gwdetail.py
@@ -3,8 +3,8 @@ from uipy_dir.gwdetail import Ui_Form
 
 
 class Call_gwdetail(QtWidgets.QWidget, Ui_Form):
-
-    def __init__(self):
+    mydata = []
+    def __init__(self,data):
         super().__init__()
         self.setupUi(self)
         self.commandLinkButton.clicked.connect(self.btnbasic)
@@ -12,6 +12,26 @@ class Call_gwdetail(QtWidgets.QWidget, Ui_Form):
         self.commandLinkButton_3.clicked.connect(self.btnimport)
         self.commandLinkButton_4.clicked.connect(self.btnelse)
         self.commandLinkButton_5.clicked.connect(self.btnanother)
+
+        self.mydata = data
+        self.display()
+
+    #显示公文详情
+    def display(self):
+        str15 = self.label_num.text()  # 发文字号
+        self.lineEdit_num.setText(self.mydata[0][2])
+
+        str16 = self.label_num_3.text()  # 公文标题
+        self.lineEdit_num_3.setText(self.mydata[0][14])
+
+        str17 = self.label_num_4.text() # 领导审核意见
+        self.textEdit_2.setText(self.mydata[0][15])
+
+        str17 = self.label_num_5.text() # 审计办领导审核意见
+        self.textEdit_4.setText(self.mydata[0][16])
+
+        str17 = self.label_num_6.text() # 办文情况说明和拟办意见
+        self.textEdit_3.setText(self.mydata[0][17])
 
     def btnbasic(self):
         self.stackedWidget.setCurrentIndex(0)
